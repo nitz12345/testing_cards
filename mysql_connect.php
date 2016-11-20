6eeccfcf1c5cd9caf392d48f9a1db270b7f4fe29
nitz12345 <info@nbenner.com>
2016-11-20 09:45:24 +0200

    Update mysql_connect.php

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,14 +1,4 @@
 <?php
-//$servername = "localhost";
-//$username = "root";
-//$password = "nitzan";
-//
-//$conn = mysqli_connect($servername, $username, $password);
-//
-//if (!$conn) {
-//	die("Connection failed: " . mysqli_connect_error());
-//}
-//echo "Connected successfully";
 $data[] = 'Nitzan';
 $data[] = 'Roni';
 $data[] = 'test';
